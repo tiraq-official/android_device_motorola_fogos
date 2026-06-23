025029de2e9ad5da3bd20944cbaae941ce1c1443
tiraq-official <203848980+tiraq-official@users.noreply.github.com>
2026-06-23 21:19:20 +0530

    added values for derpfest

--- device.mk
+++ device.mk
@@ -107,7 +107,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/fogos/fogos-vendor.mk)
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
+$(call inherit-product, vendor/motorola/motocamera/motocamera.mk)
 # Overlay Derp Maintainer
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
