6bc0d888d7d4a43820f0c2d391631aba3fb4cdb8
tiraq-official <203848980+tiraq-official@users.noreply.github.com>
2026-07-05 23:18:30 +0530

    added dolby support clone

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ clone_if_missing "https://github.com/tiraq-official/android_kernel_motorola_sm63
 
 # Additional repos (unchanged)
 clone_if_missing "https://github.com/tiraq-official/android_hardware_motorola" "lineage-23.2" "hardware/motorola"
-#clone_if_missing "https://github.com/JNWSG/hardware_dolby.git" "16.0" "hardware/dolby"
+clone_if_missing "https://github.com/tiraq-official/hardware_dolby.git" "16" "hardware/dolby"
 #clone_if_missing "https://github.com/LineageOS/android_hardware_samsung_slsi_nfc.git" "" "hardware/samsung_slsi/nfc"
 #clone_if_missing "https://github.com/JNWSG/packages_apps_ViPER4AndroidFX.git" "v4a" "packages/apps/ViPER4AndroidFX"
 clone_if_missing "https://gitea.com/JNWSG/vendor_motorola_MotCamera4.git" "16.0" "vendor/motorola/MotCamera4"
