a97ae9def3dbc6dd0dc533bfcb1bebc4201a5ab4
tiraq-official <203848980+tiraq-official@users.noreply.github.com>
2026-07-05 21:09:50 +0530

    updated device.mk and added setup.sh

--- device.mk
+++ device.mk
@@ -98,3 +98,4 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/fogos/fogos-vendor.mk)
+$(call inherit-product, vendor/motorola/MotCamera4/motcamera4.mk)
