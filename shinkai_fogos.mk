ab73e4593c76ed16538afffb62dd59be0818688c
tiraq-official <203848980+tiraq-official@users.noreply.github.com>
2026-07-21 10:24:18 +0530

    bringup for shinkai

--- shinkai_fogos.mk
+++ shinkai_fogos.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/fogos/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/shinkai/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_fogos
+PRODUCT_NAME := shinkai_fogos
 PRODUCT_DEVICE := fogos
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
