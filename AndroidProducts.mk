348f17aff1558f11c84958c49b1f93303fa9b1c7
Fortune teller 🎐🔮 <203848980+tiraq-official@users.noreply.github.com>
2026-08-09 01:08:24 +0530

    fixed lunch choice

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,6 +7,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/voltage_fogos.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_santoni-userdebug \
-    voltage_santoni-user \
-    voltage_santoni-eng
+    voltage_fogos-userdebug \
+    voltage_fogos-user \
+    voltage_fogos-eng
