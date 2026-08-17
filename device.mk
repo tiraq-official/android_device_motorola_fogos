94aee6a37291449893f6313a16cfc8a26849a57c
tiraq-official <203848980+tiraq-official@users.noreply.github.com>
2026-08-17 12:28:52 +0530

    fogos: Add SystemUI restart in restart advanced menu

--- device.mk
+++ device.mk
@@ -30,6 +30,7 @@ PRODUCT_PACKAGES += \
     ProductFrameworksResDevice \
     SystemUIResDevice \
     SettingsResDevice \
+    LineageSDKResDevice \
     WifiResDevice \
     WifiResDeviceXT2363-8 \
     WifiResDeviceXT2363-9
