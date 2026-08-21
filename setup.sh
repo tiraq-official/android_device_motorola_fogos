a29f9a9ca665a790753409e46320055caf602634
tiraq-official <203848980+tiraq-official@users.noreply.github.com>
2026-08-21 19:21:36 +0530

    fogos: changed dolby

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ clone_if_missing "https://github.com/tiraq-official/android_kernel_motorola_sm63
 
 # Additional repos (unchanged)
 clone_if_missing "https://github.com/tiraq-official/android_hardware_motorola" "lineage-23.2" "hardware/motorola"
-clone_if_missing "https://github.com/tiraq-official/hardware_dolby.git" "16" "hardware/dolby"
+clone_if_missing "https://github.com/tiraq-official/hardware_dolby_zedisp.git" "lineage-23.2" "hardware/dolby"
 clone_if_missing "https://github.com/tiraq-official/android_hardware_samsung_slsi_nfc.git" "" "hardware/samsung_slsi/nfc"
 clone_if_missing "https://github.com/tiraq-official/vendor_motorola_motocamera.git" "sixteen-qpr2" "vendor/motorola/motcamera"
 clone_if_missing "https://github.com/tiraq-official/packages_apps_ViPER4AndroidFX.git" "v4a" "packages/apps/ViPER4AndroidFX"
